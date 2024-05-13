3717ef87a1e303d0595bb4994a13ee2fb8a74655
Dongkyun Kim <donkim0122@gmail.com>
2024-05-13 15:03:35 +0900

    Setup: basic app setup

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv/config';
-import characterRouter from './routes/characters.route.js';
-import itemRouter from './routes/items.route.js';
+import express from "express";
+import dotenv from "dotenv/config";
+import characterRouter from "./routes/characters.route.js";
+import itemRouter from "./routes/items.route.js";
+import connect from "./schemas/connect.js";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+connect();
+
+// middleware
+app.use(express.json()); // convert json from req and put it in to req.body
+app.use(express.urlencoded({ extended: true })); // convert data from form content-type and put it in to req.body
+
+// default route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/character', [characterRouter]);
-app.use('/item', [itemRouter]);
+// router
+app.use("/character", [characterRouter]);
+app.use("/item", [itemRouter]);
 
 app.listen(PORT, () => {
-  console.log(PORT, '포트로 서버가 열렸어요!');
+  console.log("Server running on port:", PORT);
 });
--- routes/items.route.js
+++ routes/items.route.js
@@ -2,8 +2,19 @@ import express from 'express';
 
 const router = express.Router();
 
+/**
+ * default get, retrieves all items list
+ */
 router.get('/', (req, res) => {
-    res.send("item get called!");
+    // res.send("item get called!");
+    
+});
+
+/**
+ * creates item based on item info from req.body
+ */
+router.post('/:item', (req, res) => {
+    // 
 });
 
 export default router;
