a8e77cf3df8b229a82696ed86f4b1fc7298c13f9
Dongkyun Kim <donkim0122@gmail.com>
2024-05-15 14:01:18 +0900

    Add: package 'Joi' for validators Update: yarn.lock and package.json for 'Joi'
    Rename: items.validator.js -> items-validator.middleware.js
    
    Add: item-not-found.error.js (custom error class)
    Update: items route throws ItemNotFoundError
    Update: errorHandler middleware to handle thrown error in routers
    Update: itemValidatorJoi object with Joi validation + object schemas for Joi

---
+++ middlewares/items-validator.middleware.js
@@ -0,0 +1,112 @@
+import Joi from "joi";
+
+const isNotNumber = /[^0-9]/;
+const itemCodeSchema = Joi.object({
+  item_code: Joi.number().integer().required(),
+}).unknown(true);
+const itemNameSchema = Joi.object({
+  item_name: Joi.string().min(1).max(30).required(),
+}).unknown(true);
+const itemStatSchema = Joi.object({
+  item_stat: {
+    health: Joi.number().optional().integer(),
+    power: Joi.number().optional().integer(),
+  },
+}).unknown(true);
+const itemEquipSchema = Joi.object({
+  equip: Joi.boolean().required(),
+}).unknown(true);
+
+const itemValidatorJoi = {
+  itemCodeValidation: (req, res, next) => {
+    const target = {
+      item_code: req.body.item_code || req.params.item_code,
+    };
+
+    const validation = itemCodeSchema.validate(target);
+
+    if (validation.error) {
+      console.log("itemCodeValidation: ", validation.error.message);
+      let msg = `Invalid item_code: ${target.item_code}`;
+      return res.status(400).json({ errorMessage: msg });
+    }
+
+    next();
+  },
+  itemNameValiation: (req, res, next) => {
+    const validation = itemNameSchema.validate(req.body);
+
+    if (validation.error) {
+      console.log("itemNameValidation: ", validation.error.message);
+      let msg = `Invalid item_name: ${req.body.item_name}`;
+      return res.status(400).json({ errorMessage: msg });
+    }
+
+    next();
+  },
+  itemStatValidation: (req, res, next) => {
+    const validation = itemStatSchema.validate(req.body);
+
+    if (validation.error) {
+      console.log("itemStatValidation: ", validation.error.message);
+      let msg = `Invalid item_stat: ${item_stat}`;
+      return res.status(400).json({ errorMessage: msg });
+    }
+
+    next();
+  },
+  itemEquipValidation: (req, res, next) => {
+    const validation = itemEquipSchema.validate(req.body);
+
+    if (validation.error) {
+      console.log("itemEquipValidation: ", validation.error.message);
+      let msg = "Failed: 'equip' of type 'boolean' must be defined.";
+      return res.status(400).json({ errorMessage: msg });
+    }
+
+    next();
+  },
+};
+
+const itemValidator = {
+  itemCodeValidation: (req, res, next) => {
+    const item_code = req.body.item_code || req.params.item_code;
+    if (isNotNumber.test(item_code))
+      return res
+        .status(400)
+        .json({ errorMessage: `Invalid item_code: ${item_code}` });
+    next();
+  },
+  itemNameValiation: (req, res, next) => {
+    const { item_name } = req.body;
+    if (!item_name)
+      return res
+        .status(400)
+        .json({ errorMessage: `Invalid item_name: ${item_name}` });
+    next();
+  },
+  itemStatValidation: (req, res, next) => {
+    const { item_stat } = req.body || { item_stat: {} };
+
+    if (item_stat.health && isNotNumber.test(item_stat.health)) {
+      return res
+        .status(400)
+        .json({ errorMessage: `Invalid health: ${item_stat.health}` });
+    }
+    if (item_stat.power && isNotNumber.test(item_stat.power)) {
+      return res
+        .status(400)
+        .json({ errorMessage: `Invalid power: ${item_stat.power}` });
+    }
+
+    next();
+  },
+  itemEquipValidation: (req, res, next) => {
+    const { equip } = req.body;
+    if (equip === true || equip === false) next();
+    let msg = "Failed: 'equip' of type 'boolean' must be defined in req.body.";
+    return res.status(400).json({ errorMessage: msg });
+  },
+};
+
+export { itemValidator, itemValidatorJoi };
--- routes/items.route.js
+++ routes/items.route.js
@@ -1,62 +1,62 @@
 import express from "express";
 import Item from "../schemas/items.schema.js";
-import iv from "../middlewares/items.validator.js";
+import { itemValidatorJoi as iv } from "../middlewares/items-validator.middleware.js";
+import ItemNotFoundError from "../lib/errors/item-not-found.error.js";
 
 const router = express.Router();
 
 /**
  * Create: creates item based on the information given by form
  */
-router.post("/items", iv.itemCodeValidation, iv.itemNameValiation, iv.itemStatValidation, async (req, res) => {
-  //
-  const { item_code, item_name, item_stat } = req.body;
-  let msg = `Successfully added the item: ${item_name}`;
-  try {
-    const item = new Item({
-      item_code: item_code,
-      item_name: item_name,
-      health: item_stat.health,
-      power: item_stat.power,
-    });
-    await item.save();
-    return res.status(200).json({ message: msg });
-  } catch (err) {
-    if (err.code == 11000) {
-      msg = "Failed: Tried to post with a duplicate key.";
-      console.log(msg);
-      return res.status(500).json({ errorMessage: msg });
+router.post(
+  "/items",
+  iv.itemCodeValidation,
+  iv.itemNameValiation,
+  iv.itemStatValidation,
+  async (req, res, next) => {
+    //
+    const { item_code, item_name, item_stat } = req.body;
+    let msg = `Successfully added the item: ${item_name}`;
+    try {
+      const item = new Item({
+        item_code: item_code,
+        item_name: item_name,
+        health: item_stat.health,
+        power: item_stat.power,
+      });
+      await item.save();
+      return res.status(200).json({ message: msg });
+    } catch (err) {
+      next(err);
     }
-    msg = "An error occurred.";
-    console.log(err.name, err.code);
-    console.error(err);
-    return res.status(500).json({ errorMessage: msg });
-  }
-});
+  },
+);
 
 /**
  * Read One
  */
-router.get("/items/:item_code", iv.itemCodeValidation, async (req, res) => {
-  const item_code = req.params.item_code;
-  let msg = `Successfully found item with item_code ${item_code}`;
-  try {
-    const item = await Item.findOne({ item_code: item_code }).exec();
-    if (!item) {
-      msg = `Failed: item not found with item_code: ${item_code}`;
-      return res.status(404).json({ errorMessage: msg });
+router.get(
+  "/items/:item_code",
+  iv.itemCodeValidation,
+  async (req, res, next) => {
+    const item_code = req.params.item_code;
+    try {
+      const item = await Item.findOne({ item_code: item_code }).exec();
+
+      if (!item) throw new ItemNotFoundError();
+
+      let msg = `Successfully found item with item_code ${item_code}`;
+      return res.status(200).json({
+        message: msg,
+        item_code: item.item_code,
+        item_name: item.item_name,
+        item_stat: item.item_stat,
+      });
+    } catch (err) {
+      next(err);
     }
-    return res.status(200).json({
-      message: msg,
-      item_code: item.item_code,
-      item_name: item.item_name,
-      item_stat: item.item_stat,
-    });
-  } catch (err) {
-    console.log(err.name);
-    console.error(err);
-    return res.status(500).json({ errorMessage: "An error occurred." });
-  }
-});
+  },
+);
 
 /**
  * Read All
@@ -67,9 +67,7 @@ router.get("/items", async (req, res) => {
     if (!items) return res.status(200).json({});
     return res.status(200).json(items);
   } catch (err) {
-    console.log(err.name);
-    console.error(err);
-    return res.status(500).json({ errorMessage: "An error occurred." });
+    next(err);
   }
 });
 
@@ -82,28 +80,20 @@ router.put(
   iv.itemNameValiation,
   iv.itemStatValidation,
   async (req, res) => {
-    // res.send("item get called!");
     const item_code = req.params.item_code;
     const { item_name, item_stat } = req.body;
     let msg = `Successfully updated the item with code: ${item_code}`;
     try {
       const queryResult = await Item.updateOne(
         { item_code: item_code },
-        {
-          item_name: item_name,
-          item_stat: item_stat,
-        },
+        { item_name: item_name, ...item_stat },
       ).exec();
-      if (queryResult.matchedCount == 0) {
-        msg = "Failed: No such item.";
-        return res.status(404).json({ errorMessage: msg });
-      }
+
+      if (queryResult.matchedCount === 0) throw new ItemNotFoundError();
+
       return res.status(200).json({ message: msg });
     } catch (err) {
-      msg = "An error occurred.";
-      console.log(err.name);
-      console.error(err);
-      return res.status(500).json({ errorMessage: msg });
+      next(err);
     }
   },
 );
