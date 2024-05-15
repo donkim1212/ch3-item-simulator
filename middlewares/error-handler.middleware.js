864f557e16ee55c146c718471e4905800499f4f4
Dongkyun Kim <donkim0122@gmail.com>
2024-05-15 14:08:20 +0900

    Add: errorHandler middleware

---
+++ middlewares/error-handler.middleware.js
@@ -0,0 +1,15 @@
+const errorHandler = (err, req, res, next) => {
+  let msg = "An error occurred.";
+  console.error(err);
+
+  if (err.code == 11000) {
+    msg = "Failed: Tried to post with a duplicate key.";
+    return res.status(500).json({ errorMessage: msg });
+  } else if (err.name === "ItemNotFoundError") {
+    return res.status(404).json({ errorMessage: err.message });
+  }
+
+  return res.status(500).json({ errorMessage: msg });
+};
+
+export default errorHandler;
